46ca3e12e64b22a399bdf180c6f85d9c65174c59
Juan Minango <juan.minango@bbchain.com.br>
2025-06-05 10:26:44 -0300

    feat: initial hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "solidity-docgen";
+import "hardhat-contract-sizer";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey =
+  process.env.PRIVATE_KEY ??
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [deployerPrivateKey],
+    },
+  },
+  docgen: {
+    pages: "files",
+  },
 };
 
 export default config;
